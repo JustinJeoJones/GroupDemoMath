e74dd773338e8c65ed26a42575725c2528f53341
Justin <Justin@grandcircus.co>
2024-08-21 14:20:25 -0400

    Added Arithmetic

---
+++ GroupDemoMath/Arithmetic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupDemoMath
+{
+    internal static class Arithmetic
+    {
+        public static int Adder(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Subber(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Multipler(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Divider(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
